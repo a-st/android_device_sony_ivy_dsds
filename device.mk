a47d1ea31b19ee839caf5151ce0bc972771691e4
Adam Farden <adam@farden.cz>
2016-02-27 14:28:34 +0100

    satsuki: be compatible with CM's libnfc-nci

--- device.mk
+++ device.mk
@@ -51,10 +51,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     power.satsuki
 
-# NFC config
-PRODUCT_PACKAGES += \
-    nfc_nci.satsuki
-
 PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
